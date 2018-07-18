3af31ef1cf75adcfaf46881552bc917678b210bc
f10cz <fiocz09@gmail.com>
2018-07-18 08:57:39 +0800

    Exercise 2-4

---
+++ ex2.py
@@ -0,0 +1,11 @@
+# A comment, this is so you can read your program later.
+# Anything after # is ignored by python.
+
+print("I could have code like this.") # And the comment after is ignored.
+
+# You can also use a comment to "disable" or comment out code:
+# print("This won't run.")
+
+print("This will run.")
+
+print("Hi # there.") # Octothorpe inside a string is NOT ignored.
