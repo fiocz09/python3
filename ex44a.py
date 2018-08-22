61b780381b15a8c6a24ee3310c62b9e877086948
fiocz09 <fiocz09@gmail.com>
2018-08-22 21:03:13 +0800

    Exercise 44

---
+++ ex44a.py
@@ -0,0 +1,14 @@
+# Implicit Inheritance
+
+class Parent(object):
+    def implicit(self):
+        print("Parent Implicit()")
+
+class Child(Parent):
+    pass
+
+dad = Parent()
+son = Child()
+
+dad.implicit()
+son.implicit()
